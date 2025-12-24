27c04eb8540c64a9b931a87d32473e92f668358e
Adam Macko <adimac123456789@gmail.com>
2025-12-24 11:43:22 +0100

    Created agent class, uv modification

--- rl_first_steps.py
+++ rl_first_steps.py
@@ -4,11 +4,34 @@ import numpy as np
 import cv2
 import carla
 import math
+from collections import deque
+from keras.applications.xception import Xception
+from keras.layers import Dense, GlobalAveragePooling2D
+from keras.optimizers import Adam
+from keras.models import Model
+
 
 SHOW_PREVIEW = False
 IM_WIDTH = 640
 IM_HEIGHT = 480
 SECONDS_PER_EPISODE = 10
+REPLAY_MEMORY_SIZE = 5_000
+MIN_REPLAY_MEMORY_SIZE = 1_000
+MINIBATCH_SIZE = 16
+PREDICTION_BATCH_SIZE = 1
+TRAINING_BATCH_SIZE = MINIBATCH_SIZE // 4
+UPDATE_TARGET_EVERY = 5
+MODEL_NAME = "RL_MODEL"
+MEMORY_FRACTION = 0.8
+MIN_REWARD = -200
+
+EPISODES = 100
+
+DISCOUNT = 0.99
+EPSILON_DECAY = 0.95
+MIN_EPSILON = 0.001
+
+AGGREGATE_STATS_EVERY = 10
 
 class CarEnv:
     SHOW_CAM = SHOW_PREVIEW
@@ -17,8 +40,8 @@ class CarEnv:
     im_height = IM_HEIGHT
     front_camera = None
 
-    def __init(self):
-        self.client = carla.Client("loclahost", 2000)
+    def __init__(self):
+        self.client = carla.Client("localhost", 2000)
         self.client.set_timeout(10.0)
         self.world = self.client.get_world()
         self.blueprint_library = self.world.get_blueprint_library()
@@ -33,9 +56,9 @@ class CarEnv:
         self.actor_list.append(self.vehicle)
 
         self.rgb_cam = self.blueprint_library.find("sensor.camera.rgb")
-        self.rgb.set_attribute("image_size_x", f"{self.im_width}")
-        self.rgb.set_attribute("image_size_y", f"{self.im_height}")
-        self.rgb.set_attribute("fov", f"110")
+        self.rgb_cam.set_attribute("image_size_x", f"{self.im_width}")
+        self.rgb_cam.set_attribute("image_size_y", f"{self.im_height}")
+        self.rgb_cam.set_attribute("fov", f"110")
 
         transform = carla.Transform(carla.Location(x = 2.5, z = 0.7))
         self.sensor = self.world.spawn_actor(self.rgb_cam, transform, attach_to = self.vehicle)
@@ -96,3 +119,78 @@ class CarEnv:
 
         return self.front_camera, reward, done, None
 
+
+class DQNAgent:
+    def __init__(self):
+        self.model = self.create_model()
+        self.target_model = self.create_model()
+        self.target_model.set_weights(self.model.get_weights())
+
+        self.replay_memory = deque(maxlen = REPLAY_MEMORY_SIZE)
+
+        self.tensorboard = ModifiedTensorBoard(log_dir = f"logs/{MODEL_NAME}-{int(time.time())}")
+
+        self.target_update_counter = 0
+        self.graph = tf.get_default_graph()
+
+        self.terminate = False
+        self.last_logged_episode = 0
+        self.training_initialized = False
+
+    def create_model(self):
+        base_model = Xception(weigths = None, include_top = False, input_shape = (IM_HEIGHT, IM_WIDTH, 3))
+
+        x = base_model.output
+        x = GlobalAveragePooling2D() (x)
+        predictions = Dense(3, activation = "linear")(x)
+
+        model = Model (inputs = base_model.input, outputs = predictions)
+        model.compile(loss = "mse", optimizer = Adam(lr = 0.001), metrics = ["accuracy"])
+
+    def update_replay_memory(self, transition):
+        #transition = (current_state, action, reward, new_state, done)
+        self.replay_memory.append(transition)
+
+    def train(self):
+        if len(self.replay_memory) < MIN_REPLAY_MEMORY_SIZE:
+            return
+        minibatch = random.sample(self.replay_memory, MINIBATCH_SIZE)
+
+        current_states = np.array([transition [0] for transition in minibatch]) / 255.0
+        with self.graph.as_default():
+            current_qs_list = self.model.predict(current_states, PREDICTION_BATCH_SIZE)
+
+        new_current_states = np.array([transition [3] for transition in minibatch]) / 255.0
+        with self.graph.as_default():
+            future_qs_list = self.target_model.predict(new_current_states, PREDICTION_BATCH_SIZE)
+
+        X = []
+        y = []
+
+        for index, (current_state, action, reward, new_state, done) in enumerate(minibatch):
+            if not done:
+                max_future_q = np.max(future_qs_list[index])
+                new_q = reward + DISCOUNT * max_future_q
+            else:
+                new_q = reward
+
+            current_qs = current_qs_list [index]
+            current_qs[action] = new_q
+
+            X.append(current_state)
+            y.append(current_qs)
+
+        log_this_step = False
+        if self.tensorboard > self.last_logged_episode:
+            log_this_step = True
+            self.last_logged_episode = self.tensorboard.step
+
+        with self.graph.as_default():
+            self.model.fit(np.array(X)/255, np.array(y), batch_size = TRAINING_BATCH_SIZE, verbose = 0, shuffle = False, callback = [self.tensorboard] if log_this_step else None)
+
+        if log_this_step:
+            self.target_update_counter += 1
+
+        if self.target_update_counter > UPDATE_TARGET_EVERY:
+            self.target_model.set_weights(self.model.get_weights())
+            self.target_update_counter = 0
